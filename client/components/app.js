9542f248e1ad3b234b4bb4a935df43047b398608
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-16 21:19:27 -0700

    Started React, set up webpack and live-server start

---
+++ client/components/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  constructor(){
+    super(),
+    this.state = {
+      
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        Hello Friends
+      </div>
+    );
+  }
+
+}
+
+export default App
--- server/db/index.js
+++ server/db/index.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('mytunes', 'root', '');
 
 const Song = connection.define('song', {
-  title: Sequelize.STRING,
-  artist: Sequelize.STRING,
-  url: Sequelize.STRING,
+    title: Sequelize.STRING,
+    artist: Sequelize.STRING,
+    url: Sequelize.STRING,
   },
   {
     timestamps: false
